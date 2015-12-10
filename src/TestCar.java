c7c48716747e5719fa5d2e6589d41681e7c5c3f1
Vyacheslav.Alexeev <vnalex77@gmail.com>
2015-12-10 12:07:49 +0200

    Changes  in test Car

--- src/TestCar.java
+++ src/TestCar.java
@@ -1,18 +1,21 @@
+import java.util.ArrayList;
+
 public class TestCar
 {
         public  static  void  main(String[] args ){
             //some code
+            ArrayList AL = new ArrayList();
+
+            AL.add( "blue+yellow" );
             Car car1 = new Car();
             Car car2 = new Car();
 
-            car1.color="blue+yellow";
+            car1.color="123";
             car2.color="red";
 
                 System.out.println("First cars color  is "+ car1.color );
-                System.out.println("Hello from branch2 "+ car1.color );
-                
-                System.out.println("Second cars color is "+ car2.color );
 
         }
+
 
 }
