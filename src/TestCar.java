80cc6363aaab8bce713157056bfe0955d39dd862
Vyacheslav.Alexeev <vnalex77@gmail.com>
2015-12-10 23:41:03 +0200

    Changes  in test Car

--- src/TestCar.java
+++ src/TestCar.java
@@ -1,19 +1,55 @@
 import java.util.ArrayList;
-
+import java.util.List;
+import com.alexv.oop.*;
 public class TestCar
 {
         public  static  void  main(String[] args ){
             //some code
-            ArrayList AL = new ArrayList();
+            int intWork = 11;
+            short shortWork = 20;
+            long longWork;
+            int intLast = 12;
+            byte byteOne =10;
+            byte byteTwo =20;
+
+
+
+            List<String> AL = new ArrayList();
+            ttcl t1 = new ttcl();
+            Practic2 p2 = new Practic2();
+            Practic2 clsWork = new  Practic2();
 
             AL.add( "blue+yellow" );
+            AL.add( "bl0" );
+            AL.add( "bl1" );
+            AL.add( "bl2" );
+            AL.add( "bl3" );
             Car car1 = new Car();
             Car car2 = new Car();
 
+            //longWork= ClsWork.first( intWork );
+            //clsWork.second( shortWork );
+            //Practic2.second( shortWork );
+            // clsWork.last( intLast );
+            System.out.println( intLast);
+
+            //clsWork.overMethod( byteOne,byteTwo );
+
+            //clsWork.overMethod( intLast, intWork);
+            clsWork.difParam('h',12,9,57,99,7);
+            //System.out.println( longWork );
+          /*  for ( String at : args
+                 ) {
+                System.out.println( at);
+                }
+
+          */
+
             car1.color="123";
             car2.color="red";
-
-                System.out.println("First cars color  is "+ car1.color );
+            for (  String A  : AL  ) {
+                System.out.println("First cars color  is "+ A +t1.myName );
+            }
 
         }
 
